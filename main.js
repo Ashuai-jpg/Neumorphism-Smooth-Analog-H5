421414eec6e81c226704aeeac23c24b3f23a9500
Shuai~ <wzdsdpb@gmail.com>
2023-05-13 11:55:25 +0800

    digital clock added

--- main.js
+++ main.js
@@ -1,37 +1,46 @@
-const deg = 6
-
 const sec = document.getElementById("sec")
 const min = document.getElementById("min")
 const hr = document.getElementById("hr")
+const clockNum = document.querySelector(".digital")
 const date = new Date();
-// setInterval(() => {
-//     let date = new Date();
-//     let ss = date.getSeconds() * deg;
-//     let mm = date.getMinutes() * deg;
-//     let hh = date.getHours() * 30;
-
-//     sec.style.transform = `rotateZ($(ss)deg)` 
-//     this.sec.style.transform = `rotateZ(${ss}deg)`
-//     this.min.style.transform = `rotateZ(${mm}deg)`
-//     this.hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`
-//     console.log(`every angel: hr:${hh}, min:${mm}, sec:${ss}`)
-//     let ms = date.getMilliseconds()
-//     console.log(ms)
-// }, 10);
 
 
 /** 
  * smooth hands
 */
-
 function moveHands() {
     const secs = date.getSeconds(),
-        mins = date.getMinutes() * 60,
-        hours = date.getHours() * 3600;
-        sec.style.animationDelay= `-${secs}s`;
-        min.style.animationDelay= `-${mins}s`;
-        hr.style.animationDelay= `-${hours}s`;
-    console.log(`every angel: hr:${sec.style}, min:${min.style}, sec:${hr.style}`)
-}  
-moveHands()
+        mins = date.getMinutes() * 60 + date.getSeconds(),
+        hours = date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds();
+    sec.style.animationDelay = `-${secs}s`;
+    min.style.animationDelay = `-${mins}s`;
+    hr.style.animationDelay = `-${hours}s`;
+}
+function digiFormatter() {
+    const time = new Date();
+    let ss = time.getSeconds();
+    let mm = time.getMinutes();
+    let hh = time.getHours();
+    let dayNight = "AM"
+    let digitHours
+    if (hh > 12) {
+        dayNight = "PM";
+        digitHours = hours - 12;
+    }
+    if (ss < 10) {
+        ss = '0' + ss;
+    }
+    if (mm < 10) {
+        mm = '0' + mm
+    };
+    if (hh < 10) {
+        hh = '0' + hh
+    };
+    clockNum.textContent = `${hh} : ${mm} : ${ss} : ${dayNight}`
+    console.log(clockNum.textContent)
+}
+setInterval(() => {
+    moveHands()
+    digiFormatter()
+}, 1000);
 
